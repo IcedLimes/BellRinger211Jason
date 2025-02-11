9c7825a66845cef8898d84811934dc6df507af2c
Jason Zeng <101027720+IcedLimes@users.noreply.github.com>
2025-02-11 13:49:52 -0800

    Update time.py

--- time.py
+++ time.py
@@ -1,15 +1,10 @@
-import time
-hour_end=14
-minute_end = 19
+import time 
+hour_end=14 
+minute_end = 19 
 
 while True:
     current_time = time.localtime()
     current_hour = current_time.tm_hour
-    current_minute = current_time. tm_min
-    remaining_hours = hour_end - current_hour
-    remaining_minutes = minute_end -current_minute
-
-    if remaining_minutes < 0:
-        remaining_hours -= 1
-        remaining_minutes += 60
-    print(f"Time remaining until class ends: {remaining_hours} hours and {remaining_minutes} minutes")
+    current_minute = current_time. tm_min 
+    remaining_minutes = (hour_end - current_hour) * 60 + (minute_end - current_minute)
+    print(f"Time remaining until class ends: {remaining_minutes} minutes")
